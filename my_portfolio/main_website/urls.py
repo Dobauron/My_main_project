c7f245a7eab32f36bf92a47bdce91198c4715058
dobromir <safetysafetystrazak@gmail.com>
2021-12-28 08:46:43 +0100

    Prepare IDE on Ubuntu, add postgres as a main DB, and create new DB for project

--- my_portfolio/main_website/urls.py
+++ my_portfolio/main_website/urls.py
@@ -3,7 +3,7 @@ from .views import index, Who_amI
 
 urlpatterns=[
     path('', index, name="main_website"),
-    path('Who_amI', Who_amI),
+    path('Who_amI', Who_amI, name="Who_amI"),
 
 ]
 
--- my_portfolio/main_website/views.py
+++ my_portfolio/main_website/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your views here.
+app_name = "main_website"
 def index(request):
     return render(request, 'index.html')
 
 def Who_amI(request):
-    return render(request, 'who_amI.html')
+    return render(request, 'Who_amI.html')
 
 
-from django.db import models
-from django.utils import timezone
-from django.contrib.auth.models import User
 
 
 #urls.py - my_blog
